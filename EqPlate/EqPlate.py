0b59e3cb267c368dc4f181c597703d9d0dd190e0
Matthew Pancia <msp@themsp.org>
2022-08-14 12:48:40 -0700

    Update name, remove README

--- EqPlate/EqPlate.py
+++ EqPlate/EqPlate.py
@@ -1,5 +1,3 @@
-
-
 import Live
 from _Framework.ControlSurface import ControlSurface
 from _Framework.InputControlElement import *
@@ -23,17 +21,18 @@ from .MIDI_Map import *
 # MIDI_PB_TYPE = 2
 
 
-class YourControllerName(ControlSurface):   # Make sure you update the name
-    __doc__ = " Script for YourControllerName in APC emulation mode "   # Make sure you update the name
+class EqPlate(ControlSurface):
+    __doc__ = " Script for YourControllerName in APC emulation mode "
 
     _active_instances = []
 
     def _combine_active_instances():
         track_offset = 0
         scene_offset = 0
-        for instance in YourControllerName._active_instances:   # Make sure you update the name
+        for instance in EqPlate._active_instances:
             instance._activate_combination_mode(track_offset, scene_offset)
             track_offset += instance._session.width()
+
     _combine_active_instances = staticmethod(_combine_active_instances)
 
     def __init__(self, c_instance):
@@ -68,14 +67,16 @@ class YourControllerName(ControlSurface):   # Make sure you update the name
         ControlSurface.disconnect(self)
 
     def _do_combine(self):
-        if self not in YourControllerName._active_instances:    # Make sure you update the name
-            YourControllerName._active_instances.append(self)   # Make sure you update the name
-            YourControllerName._combine_active_instances()  # Make sure you update the name
+        if self not in EqPlate._active_instances:
+            EqPlate._active_instances.append(self)
+            EqPlate._combine_active_instances()
 
     def _do_uncombine(self):
-        if (self in YourControllerName._active_instances) and YourControllerName._active_instances.remove(self):    # Make sure you update the name
+        if (self in EqPlate._active_instances) and EqPlate._active_instances.remove(
+            self
+        ):
             self._session.unlink()
-            YourControllerName._combine_active_instances()  # Make sure you update the name
+            EqPlate._combine_active_instances()
 
     def _activate_combination_mode(self, track_offset, scene_offset):
         if TRACK_OFFSET != -1:
@@ -86,45 +87,68 @@ class YourControllerName(ControlSurface):   # Make sure you update the name
 
     def _setup_session_control(self):
         is_momentary = True
-        self._session = SpecialSessionComponent(TSB_X, TSB_Y)   # Track selection box size (X,Y) (horizontal, vertical).
-        self._session.name = 'Session_Control'
-        self._session.set_track_bank_buttons(self._note_map[SESSIONRIGHT], self._note_map[SESSIONLEFT])
-        self._session.set_scene_bank_buttons(self._note_map[SESSIONDOWN], self._note_map[SESSIONUP])
-        self._session.set_select_buttons(self._note_map[SCENEDN], self._note_map[SCENEUP])
+        self._session = SpecialSessionComponent(
+            TSB_X, TSB_Y
+        )  # Track selection box size (X,Y) (horizontal, vertical).
+        self._session.name = "Session_Control"
+        self._session.set_track_bank_buttons(
+            self._note_map[SESSIONRIGHT], self._note_map[SESSIONLEFT]
+        )
+        self._session.set_scene_bank_buttons(
+            self._note_map[SESSIONDOWN], self._note_map[SESSIONUP]
+        )
+        self._session.set_select_buttons(
+            self._note_map[SCENEDN], self._note_map[SCENEUP]
+        )
         # range(tsb_x) is the horizontal count for the track selection box
-        self._scene_launch_buttons = [self._note_map[SCENELAUNCH[index]] for index in range(TSB_X)]
+        self._scene_launch_buttons = [
+            self._note_map[SCENELAUNCH[index]] for index in range(TSB_X)
+        ]
         # range(tsb_y) Range value is the track selection
-        self._track_stop_buttons = [self._note_map[TRACKSTOP[index]] for index in range(TSB_Y)]
+        self._track_stop_buttons = [
+            self._note_map[TRACKSTOP[index]] for index in range(TSB_Y)
+        ]
         self._session.set_stop_all_clips_button(self._note_map[STOPALLCLIPS])
         self._session.set_stop_track_clip_buttons(tuple(self._track_stop_buttons))
-        self._session.selected_scene().name = 'Selected_Scene'
+        self._session.selected_scene().name = "Selected_Scene"
         self._session.selected_scene().set_launch_button(self._note_map[SELSCENELAUNCH])
         self._session.set_slot_launch_button(self._note_map[SELCLIPLAUNCH])
-        for scene_index in range(TSB_Y):    # Change range() value to set the vertical count for track selection box
+        for scene_index in range(
+            TSB_Y
+        ):  # Change range() value to set the vertical count for track selection box
             scene = self._session.scene(scene_index)
-            scene.name = 'Scene_' + str(scene_index)
+            scene.name = "Scene_" + str(scene_index)
             button_row = []
             scene.set_launch_button(self._scene_launch_buttons[scene_index])
             scene.set_triggered_value(2)
-            for track_index in range(TSB_X):    # Change range() value to set the horizontal count for track selection box
+            for track_index in range(
+                TSB_X
+            ):  # Change range() value to set the horizontal count for track selection box
                 button = self._note_map[CLIPNOTEMAP[scene_index][track_index]]
                 button_row.append(button)
                 clip_slot = scene.clip_slot(track_index)
-                clip_slot.name = str(track_index) + '_Clip_Slot_' + str(scene_index)
+                clip_slot.name = str(track_index) + "_Clip_Slot_" + str(scene_index)
                 clip_slot.set_launch_button(button)
         self._session_zoom = SpecialZoomingComponent(self._session)
-        self._session_zoom.name = 'Session_Overview'
-        self._session_zoom.set_nav_buttons(self._note_map[ZOOMUP], self._note_map[ZOOMDOWN], self._note_map[ZOOMLEFT], self._note_map[ZOOMRIGHT])
+        self._session_zoom.name = "Session_Overview"
+        self._session_zoom.set_nav_buttons(
+            self._note_map[ZOOMUP],
+            self._note_map[ZOOMDOWN],
+            self._note_map[ZOOMLEFT],
+            self._note_map[ZOOMRIGHT],
+        )
 
     def _setup_mixer_control(self):
 
         is_momentary = True
         self._mixer = SpecialMixerComponent(8)
-        self._mixer.name = 'Mixer'
-        self._mixer.master_strip().name = 'Master_Channel_Strip'
+        self._mixer.name = "Mixer"
+        self._mixer.master_strip().name = "Master_Channel_Strip"
         self._mixer.master_strip().set_select_button(self._note_map[MASTERSEL])
-        self._mixer.selected_strip().name = 'Selected_Channel_Strip'
-        self._mixer.set_select_buttons(self._note_map[TRACKRIGHT], self._note_map[TRACKLEFT])
+        self._mixer.selected_strip().name = "Selected_Channel_Strip"
+        self._mixer.set_select_buttons(
+            self._note_map[TRACKRIGHT], self._note_map[TRACKLEFT]
+        )
         self._mixer.set_crossfader_control(self._ctrl_map[CROSSFADER])
         self._mixer.set_prehear_volume_control(self._ctrl_map[CUELEVEL])
         self._mixer.master_strip().set_volume_control(self._ctrl_map[MASTERVOLUME])
@@ -135,20 +159,26 @@ class YourControllerName(ControlSurface):   # Make sure you update the name
             # My guess is that altering the range here will allow you to alter the range of mixer tracks
             # So if you had a 16 fader mixer, this would come in handy.
             strip = self._mixer.channel_strip(track)
-            strip.name = 'Channel_Strip_' + str(track)
+            strip.name = "Channel_Strip_" + str(track)
             strip.set_arm_button(self._note_map[TRACKREC[track]])
             strip.set_solo_button(self._note_map[TRACKSOLO[track]])
             strip.set_mute_button(self._note_map[TRACKMUTE[track]])
             strip.set_select_button(self._note_map[TRACKSEL[track]])
             strip.set_volume_control(self._ctrl_map[TRACKVOL[track]])
             strip.set_pan_control(self._ctrl_map[TRACKPAN[track]])
-            strip.set_send_controls((self._ctrl_map[TRACKSENDA[track]], self._ctrl_map[TRACKSENDB[track]], self._ctrl_map[TRACKSENDC[track]]))
+            strip.set_send_controls(
+                (
+                    self._ctrl_map[TRACKSENDA[track]],
+                    self._ctrl_map[TRACKSENDB[track]],
+                    self._ctrl_map[TRACKSENDC[track]],
+                )
+            )
             strip.set_invert_mute_feedback(True)
 
     def _setup_device_and_transport_control(self):
         is_momentary = True
         self._device = DeviceComponent()
-        self._device.name = 'Device_Component'
+        self._device.name = "Device_Component"
         device_bank_buttons = []
         device_param_controls = []
         for index in range(8):
@@ -159,18 +189,22 @@ class YourControllerName(ControlSurface):   # Make sure you update the name
         if None not in device_param_controls:
             self._device.set_parameter_controls(tuple(device_param_controls))
         self._device.set_on_off_button(self._note_map[DEVICEONOFF])
-        self._device.set_bank_nav_buttons(self._note_map[DEVICEBANKNAVLEFT], self._note_map[DEVICEBANKNAVRIGHT])
+        self._device.set_bank_nav_buttons(
+            self._note_map[DEVICEBANKNAVLEFT], self._note_map[DEVICEBANKNAVRIGHT]
+        )
         self._device.set_lock_button(self._note_map[DEVICELOCK])
         self.set_device_component(self._device)
 
         detail_view_toggler = DetailViewControllerComponent()
-        detail_view_toggler.name = 'Detail_View_Control'
+        detail_view_toggler.name = "Detail_View_Control"
         detail_view_toggler.set_device_clip_toggle_button(self._note_map[CLIPTRACKVIEW])
         detail_view_toggler.set_detail_toggle_button(self._note_map[DETAILVIEW])
-        detail_view_toggler.set_device_nav_buttons(self._note_map[DEVICENAVLEFT], self._note_map[DEVICENAVRIGHT])
+        detail_view_toggler.set_device_nav_buttons(
+            self._note_map[DEVICENAVLEFT], self._note_map[DEVICENAVRIGHT]
+        )
 
         transport = SpecialTransportComponent()
-        transport.name = 'Transport'
+        transport.name = "Transport"
         transport.set_play_button(self._note_map[PLAY])
         transport.set_stop_button(self._note_map[STOP])
         transport.set_record_button(self._note_map[REC])
@@ -202,7 +236,12 @@ class YourControllerName(ControlSurface):   # Make sure you update the name
             pad = []
             for row in range(4):
                 for col in range(4):
-                    pad = (col, row, DRUM_PADS[row*4 + col], PADCHANNEL,)
+                    pad = (
+                        col,
+                        row,
+                        DRUM_PADS[row * 4 + col],
+                        PADCHANNEL,
+                    )
                     self._pads.append(pad)
             self.set_pad_translations(tuple(self._pads))
 
@@ -210,15 +249,17 @@ class YourControllerName(ControlSurface):   # Make sure you update the name
         is_momentary = True
         for note in range(128):
             button = ButtonElement(is_momentary, MESSAGETYPE, BUTTONCHANNEL, note)
-            button.name = 'Note_' + str(note)
+            button.name = "Note_" + str(note)
             self._note_map.append(button)
-        self._note_map.append(None)     # add None to the end of the list, selectable with [-1]
+        self._note_map.append(
+            None
+        )  # add None to the end of the list, selectable with [-1]
         if MESSAGETYPE == MIDI_CC_TYPE and BUTTONCHANNEL == SLIDERCHANNEL:
             for ctrl in range(128):
                 self._ctrl_map.append(None)
         else:
             for ctrl in range(128):
                 control = SliderElement(MIDI_CC_TYPE, SLIDERCHANNEL, ctrl)
-                control.name = 'Ctrl_' + str(ctrl)
+                control.name = "Ctrl_" + str(ctrl)
                 self._ctrl_map.append(control)
             self._ctrl_map.append(None)
---
+++ EqPlate/__init__.py
@@ -0,0 +1,7 @@
+
+import Live
+from .EqPlate import EqPlate
+
+def create_instance(c_instance):
+    ' Creates and returns the EqPlate script '
+    return EqPlate(c_instance)
